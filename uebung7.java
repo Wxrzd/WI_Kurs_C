25d152588a192595cde81c8bd399a00785441b40
HereToDoMyLearning <115903370+HereToDoMyLearning@users.noreply.github.com>
2022-11-29 14:54:11 +0100

    Create uebung7.java

---
+++ uebung7.java
@@ -0,0 +1,21 @@
+package #;
+
+public class # {
+  
+  // # (Hashtag) bitte durch jeweiligen Classname und Packagename ersetzen!!!
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(#.getMaximum(9,5));
+		# myTMObj =new #();
+		System.out.println(myTMObj.getMaximum(44,100));
+
+	}
+	public static int getMaximum(int a, int b) {
+		if (a >= b) {
+			return a;
+		}else {
+			return b;
+		}
+	}
+}
